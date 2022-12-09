655fede1ba5ef1268b29c10583263c343917c2b6
vikraam27 <ramvik36@gmail.com>
2022-12-09 21:52:38 +0700

    change directory script

---
+++ jenkins/scripts/deploy.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo "change directory"
+cd a428-cicd-labs
+
+echo "pull repository"
+git pull origin react-app
+
+echo "install all package"
+npm install
+
+echo "build react-app"
+npm run build
+
+echo "restart server"
+pm2 restart react-app
+
+echo "wait 1 minutes"
+sleep 1m
